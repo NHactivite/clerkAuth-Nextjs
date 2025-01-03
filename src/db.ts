aa9e1db9525e8d304eed26387137ddf7c85e2da8
NIKHIL HAZARIKA <nikhilhazarika9@gmail.com>
2025-01-03 16:58:53 +0530

    remove useles

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,15 @@
 import mongoose from "mongoose";
 
-const ConnectDB=async()=>{
-    const url=process.env.MONGO_URL
-    mongoose.connect(url!).then(()=>console.log("database Conntected")).catch((e)=>console.log(e))
-}
+// const ConnectDB=async()=>{
+//     const url=process.env.MONGO_URL
+//     mongoose.connect(url!).then(()=>console.log("database Conntected")).catch((e)=>console.log(e))
+// }
+export  const ConnectDB=()=>{
+    mongoose.connect(process.env.MONGO_URL!,{
+        dbName:"Ecomerce",
+    }).then(c=>console.log(`database connected ${c.connection.host}`)).catch(e=>console.log(e));
+   
+};
 
 export default ConnectDB;
 
